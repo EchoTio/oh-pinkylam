7b04947ed5b654daafe0691618d2ff6c6c6e278b
handexing <908716835@qq.com>
2017-06-09 13:51:20 +0800

    配置thymeleaf

--- src/main/java/com/pinkylam/blog/controller/UserController.java
+++ src/main/java/com/pinkylam/blog/controller/UserController.java
@@ -4,10 +4,11 @@ import com.pinkylam.blog.dao.UserDao;
 import com.pinkylam.blog.entity.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping(value = "user")
 public class UserController {
 
@@ -18,5 +19,11 @@ public class UserController {
 	public String deleteUser() {
 		User user = userDao.getOne(1L);
 		return user.getName();
+	}
+
+	@RequestMapping("/index")
+	public String index(Model model) {
+		model.addAttribute("name", "handx");
+		return "index";
 	}
 }
