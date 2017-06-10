0bf2c2b2b7d32c389cf99a8e40e1619aecca56e5
handexing <908716835@qq.com>
2017-06-10 21:51:19 +0800

    文章列表展示界面开发

--- README.md
+++ README.md
@@ -12,6 +12,7 @@
 ## 更新日志
 
 - 初始化项目,集成jpa,配置thymeleaf模板引擎,完成登录界面和首页面开发(*2017-06-09*)
+- 文章列表展示界面开发(*2017-06-10*)
 
 ## 效果预览
 
@@ -23,5 +24,8 @@
 
 ![登录界面](https://github.com/handexing/oh-pinkylam/raw/master/img/index.png)
 
+### 文章列表界面
+
+![登录界面](https://github.com/handexing/oh-pinkylam/raw/master/img/articleList.png)
 
 > 觉得不错的朋友可以点下star,watch,fork也算是对我的鼓励了。
---
+++ src/main/java/com/pinkylam/blog/controller/ArticleController.java
@@ -0,0 +1,20 @@
+package com.pinkylam.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Pinky Lam 908716835@qq.com
+ * @date 2017年6月10日 下午9:08:08
+ */
+
+@Controller
+@RequestMapping(value = "article")
+public class ArticleController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "backend/articleList";
+	}
+
+}
